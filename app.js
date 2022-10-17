7421596144d83afa0ab19f43fa0524fbc02fc610
(JongHyeonMoon) <(zin354@gmail.com)>
2022-10-17 09:39:31 +0900

    server.js 분리

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const cookieParser = require('cookie-parser');
 
 const app = express();
 const http = Http.createServer(app);
-const port = process.env.EXPRESS_PORT || 3000;
 
 app.use(cookieParser());
 app.use(express.json());
@@ -14,8 +13,4 @@ app.use('/', require('./routes/users.routes'));
 app.use('/posts', require('./routes/posts.routes'));
 app.use('/comments', require('./routes/comments.routes'));
 
-http.listen(port, () => {
-    console.log(`Start listen Server: ${port}`);
-  });
-  
-  module.exports = http;
+module.exports = http;
---
+++ server.js
@@ -0,0 +1,6 @@
+const http = require('./app');
+const port = process.env.EXPRESS_PORT || 3000;
+
+http.listen(port, () => {
+    console.log(` I love you, ${port}. ${port}만큼 사랑해. `);
+  });
